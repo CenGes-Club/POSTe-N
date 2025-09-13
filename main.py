51fd0adbf3a7707a01b8140e69010be55ad3fc60
Nick Anthony Miras <mirasnickanthony@gmail.com>
2025-09-13 13:46:12 +0800

    added documentation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import serial
-from configs import parse_serial_config
+from configs import parse_serial_config, verify_unique_xml_value
 from time import sleep
 from commands import write_to_serial, AT
 from crccheck.crc import Crc16Modbus
@@ -7,8 +7,6 @@ import csv
 from struct import unpack
 from datetime import datetime
 
-from scratch.buzzer import notify_buzzer
-from scratch.generics import get_last_n_rows_csv
 
 ## Paths
 DATA_LOG_PATH = '/home/postekit/POSTe/data_log.csv'
@@ -22,9 +20,9 @@ COUNT = 0
 
 
 # Initialize Variables
-DSG_PORT = serial.Serial(**parse_serial_config('config.xml', 'dsg'))
-DRRG_PORT = serial.Serial(**parse_serial_config('config.xml', 'drrg'))
-LORA_PORT = serial.Serial(**parse_serial_config('config.xml', 'lora'))
+DSG_PORT = serial.Serial(**parse_serial_config('config.xml', 'dsg', verify_unique_xml_value))
+DRRG_PORT = serial.Serial(**parse_serial_config('config.xml', 'drrg', verify_unique_xml_value))
+LORA_PORT = serial.Serial(**parse_serial_config('config.xml', 'lora', verify_unique_xml_value))
 
 
 DSG_PORT.rs485_mode = serial.rs485.RS485Settings(
@@ -73,14 +71,22 @@ def setup():
 
 
 def get_drrg_data(initial_time):  # TODO: Prettify
+    """
+
+    Args:
+        initial_time:
+
+    Returns:
+        `tuple` of len 2
+    """
     data = get_data_from_port(DRRG_PORT, DRRG_COMM_0)
     if len(data) == 37:  # TODO: Ask Boss James Why This is Specifically 37
         if do_crc_check(data) != 0:
             COUNT += 1  # TODO: This must be changed, pls
-            return
+            return list(), True
 
         rain_data, accu_data = bytearray(0), bytearray(0)
-        rain_temp, accu_temp = bytearray(4), bytearray(4)
+        rain_temp, accu_temp = bytearray(4), bytearray(4)  # TODO: Should this be called accu?
 
         ##-> TODO: This could be turned into a function, but...
         rain_data += data[27:31]
@@ -105,34 +111,43 @@ def get_drrg_data(initial_time):  # TODO: Prettify
             writer = csv.writer(f)
             csv_write = [initial_time, str(rain_data_f), str(accu_data_f), "DRRG"]
             writer.writerow(csv_write)
-        return csv_write[0:3]
+        return csv_write[1:3], False
     print("ERROR: No communication with DRRG!")
-    return None
+    return list(), True
 
 
 def get_dsg_data(initial_time):
+    """
+
+
+    Args:
+        initial_time:
+
+    Returns:
+        `tuple` of len 1
+    """
     data = get_data_from_port(DSG_PORT, DSG_COMM_0)
     if data is None:
         print("ERROR: No communication with DSG!")
     if do_crc_check(data) != 0:
-        return None
+        return list(), True
 
-    data_int = int.from_bytes(data[4:5], "big")
-    print("Water level: %d cm" % data_int)
+    water_level = int.from_bytes(data[4:5], "big")
+    print("Water level: %d cm" % water_level)
 
     with open(DATA_LOG_PATH, 'a') as f:
         writer = csv.writer(f)
-        csv_write = [initial_time, str(data_int), " cm", "DSG"]
+        csv_write = [initial_time, str(water_level), " cm", "DSG"]
         writer.writerow(csv_write)
 
     ###Warning Lights and Alarm
-    notify_buzzer(data_int)
+    notify_buzzer(water_level)
     ###
 
-    return csv_write[0:1]
+    return csv_write[1:2], False
 
 
-def send_data_to_lora(start_time):
+def send_data_to_lora(start_time, data):
     """ This should obtain the last n rows of data from the data log path
     and transform it into the 'specified' data format.
     :param start_time: `datetime` determines which row to start truncating from
@@ -141,16 +156,18 @@ def send_data_to_lora(start_time):
     ##
     # TODO: Process :param start_time:
     ##
-    data_to_send = transform_data(data)
+    data_to_send = transform_data(*data)
     write_to_serial(LORA_PORT, AT.MSG, data_to_send)
 
 
-def transform_data(data): # TODO: Function Not Complete
+def transform_data(rain_data, rain_accu, water_level): # TODO: Function Not Complete
     """This should transform `data` and turn it into LoRa compatible characters.
-    :param data:`list[list[Any]]` A list of rows from the csv file.
+    :param rain_data:`float`
+    :param rain_accu:`float`
+    :param water_level:`float`
     :return:
     """
-    return data
+    return
 
 
 def main():
@@ -160,25 +177,13 @@ def main():
     start_time = datetime.now()  # this should fix race condition
     data_to_transmit = []
     while DSG_PORT.is_open and DSG_PORT.is_open:
-        if inc == 10:  # after 10 minutes, send the data to LoRa
-            send_data_to_lora(initial_time)
-            inc = 0
-            start_time = datetime.now()
-            continue
 
-        drrg_data = get_drrg_data(start_time)
-        dsg_data = get_dsg_data(start_time)
-        if drrg_data is not None and dsg_data is not None:
-            # transmit the data here
-            for
-            pass
-        else:
-            if drrg_data is None:
-                print("ERROR: No data with DRRG!")
-            if dsg_data is None:
-                print("ERROR: No data with DSG!")
-        sleep(58)
+        drrg_data, has_error_1  = get_drrg_data(start_time)
+        dsg_data, has_error_2 = get_dsg_data(start_time)
+
+        sleep(60)
         inc += 1
+        start_time = datetime.now()
 
 
 if __name__ == '__main__':
